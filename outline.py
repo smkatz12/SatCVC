1eb3e1eb4b9e2fbd98de708af1b92802618056ef
Somrita Banerjee <somrita@stanford.edu>
2019-02-24 15:36:49 -0800

    Added outline

---
+++ outline.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+main():
+    figureOutEnv()
+    Lloyd()
+
+figureOutEnv():
+    computePhi()
+
+Lloyd(pInit, phi):
+    computeVi()
+    computeCVi()
+    update_pi()
+    if pi==CVi:
+        end
+
+arcDistance(p,q,R):
+
+
+computePhi():
+    do this everywhere
+    normal = computeNormal(theta,phi)
+    phi = abs(normal.dot(linetocenter))
+
+computeNormal(theta,phi):
+
+computeVi():
+    use arcDistance(p,q,R)
+    should return arrays of [theta, psi]
+
+computeCVi():
+    call computePhi()
+
+update_pi(): 
+    pidot = K*(Cvi-pi)
+    pi_new = pi + pidot*dt 
+
