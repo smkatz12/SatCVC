25c385c11bd026642086feb4edcec60ccdc79e3e
Sydney Katz <sydneykatz@DN51u5k4.SUNet>
2019-02-27 00:07:45 -0800

    Set up outer shell of main functionality

---
+++ create_environment.jl
@@ -0,0 +1,96 @@
+"""
+create_environment.jl
+- determines the environment for the sensor coverage problem by calculating
+ϕ(q) based on the object geometry
+
+General notes:
+ψ defines elevation (defined like latitude ranging from -90 to 90 degrees)
+λ defines azimuth (defined like longitude ranging from -180 to 180 degrees)
+- defined like lat and lon so that we can use haversine formula as distance function
+
+Here is how we are going to define our object:
+- It is a rectangular prism centered at (0,0)
+- Front face = all x positive, back face = all x negative
+- Right face = all y positive, left face = all y negative
+- Top face = all z positive, bottom face = all z negative
+- All vectors specifying parameters will then be in order of 
+[Front, back, right, left, top, bottom]
+- To fully specify our geometry we just need the length (corresponds to y-direction), 
+width (corresponds to x-direction), and height (corresponds to z-direction)
+"""
+
+"""
+function calcϕ
+	- Calculate the sensory function at each point in the discretized 
+	space based on the object geometry (assuming some form of rectangular 
+	prism for now)
+
+	INPUTS:
+	- ψdisc: discretized ψ points
+	- λdisc: discretized λ points
+	- l: length of the sensed object
+	- w: width of the sensed object
+	- h: height of the sensed object
+	- r: radius of the sphere to place the satellites on
+
+	OUTOUTS:
+	- ϕ: matrix that constains a value for each ϕ(ψ,λ)
+	ψ varies in the rows while λ varies in the columns
+	(e.g. ϕ[1,2] is ϕ of the first discretized value of ψ and the second discretized value of λ)
+"""
+function calcϕ(ψdisc::Array{Real}, λdisc::Array{Real}, l::Real, w::Real, h::Real, r::Real)
+	# Initialize ϕ
+	ϕ = zeros(length(ψdisc),length(θdisc))
+	for i = 1:length(ψdisc)
+		for j = 1:length(λdisc)
+			# Get vector from point on sphere to center (the origin) in intertial cartesian coordinates
+			vc = get_vector_to_center(ψdisc[i], λdisc[i], r)
+			# Figure out which surface normal we care about based on the vector to the origin
+			n = choose_surface_normal(vc, l, w, h)
+			# Fill in ϕ using the dot product (normalize vector to center first)
+			vc_norm = vc./norm(vc)
+			ϕ[i,j] = abs(dot(vc_norm, n))
+		end
+	end
+	return ϕ
+end
+
+"""
+function get_vector_to_center
+	- calculate the vector from the point on the sphere defined by
+	ψ and λ to the center of the sphere (the origin)
+	- This should just be a matter of getting the cartesian coordinates
+	(careful not typical spherical coord conversion since using lat/lon coord system)
+
+	INPUTS:
+	- ψ: latitude
+	- λ: longitude
+	- r: radius of the sphere
+	
+	OUTPUTS:
+	- vc: vector to the origin
+"""
+function get_vector_to_center(ψ::Real, λ::Real, r::Real)
+	vc = [0 0 0] # Fill this in with the correct values!
+	return vc
+end
+
+"""
+function choose_surface_normal
+	- returns the surface normal of the surface that is being observed
+	from a particular position on the sphere
+	- should be the first surface that vc crosses
+
+	INPUTS:
+	- vc: vector from the point on the sphere to the center (origin)
+	- l: length of the object
+	- w: width of the object
+	- h: height of the object
+	
+	OUTPUTS:
+	- n: normal vector to the surface we care about
+"""
+function choose_surface_normal(vc::Array{Real,1}, l::Real, w::Real, h::Real)
+	n = [0 0 0] # Fill this in with the correct values!
+	return n
+end
---
+++ satCVC.jl
@@ -0,0 +1,140 @@
+"""
+satCVC.jl
+- Implements the functions necessary to perform centroidal Voronoi coverage
+control to sense a space object
+
+Things I am currently concerned about:
+Angle wrapover in the control law (this is going to be super weird when it happens
+and can probably cause some undesired behavior of the system)
+"""
+
+"""
+function simulate_cvc
+	- main function to simulate satellite coverage
+	- ******* A note on the compute_voronoi function:
+		- If this were truly distributed, its input would be only the positions of its neighbors
+		- We are not losing generality when we just input all positions because
+		the positions any robots that are not neighbors will be irrelavent in the calculation
+	- TODO: Add functionality to keep track of p on each iteration in a 3D array so that we can plot
+	their entire trajectories instead of just final position
+	- Anything after the semicolon is an optional input with a default value
+
+	INPUTS:
+	- p₀: initial positions (ψ, λ) for the robots
+		- Each row is a different robot (so columns are ψ and λ)
+	- ψdisc: discretized ψ points
+	- λdisc: discretized λ points
+	- k: controller gain (this will need tuning)
+	-------------
+	- max_iter: maximum number of time steps
+	- tol: magnitude of change in position must be smaller than this to declare convergence
+	- dt: time step
+
+	OUTPUTS:
+	- p: final position of the robots (set up the same as p0) 
+"""
+function simulate_cvc(p₀::Array{Real,2}, ψdisc::Array{Real,1}, λdisc::Array{Real,1}, k::Real;
+	max_iter = 100, tol = 0.1, dt = 0.1)
+	p = p₀
+	for j = 1:max_iter # Using j so that I can use i for the robots (sorry this is backwards)
+		# Initialize vector to fill with new positions
+		pnew = zeros(size(p₀))
+
+		# Compute the voronoi region of each robot
+		V = compute_voronoi(p, ψdisc, λdisc)
+
+		# Iterate though each robot
+		for i = 1:size(p,1)
+			# Next compute the centroid
+			CVᵢ = compute_centroid(V[i])
+			
+			# Find ṗᵢ
+			ṗᵢ = k*(CVᵢ-p[i,:])
+
+			# Update p
+			pnew[i,:] = p[i,:] + ṗᵢ*dt
+		end
+
+		# Check termination
+		if norm(pnew - p) < tol
+			return p
+		end
+
+		# Set up for next iteration
+		p = p_new
+	end
+
+	println("Hit maximum iterations before converging :(. Returning final position anyway ...")
+	return p
+end
+
+"""
+function compute_voronoi
+	- return all of the discretized points in the voronoi region of Q
+	(according to our geodesic distance function)
+	- This should fully work once d is implemented (no need for further code)
+
+	INPUTS:
+	- p: current robot positions
+	- ψdisc: discretized ψ points
+	- λdisc: discretized λ points
+
+	OUTPUTS:
+	- V: Dictionary mapping robot index to array of points in Voronoi region
+"""
+function compute_voronoi(p::Array{Real,2}, ψdisc::Array{Real,1}, λdisc::Array{Real,1})
+	V = Dict{Int64,Array{Real,2}}()
+	for i = 1:length(ψdisc)
+		for j = 1:length(λdisc)
+			min_dist = Inf
+			closest_robot = 0
+			for k = 1:size(p,1) # num robots
+				dist = d([ψdisc[i],λdisc[j]], p[k,:]) # Distance between discretized point and robot k
+				if dist < min_dist
+					min_dist = dist
+					closest_robot = k
+				end
+			end
+			if haskey(V, closest_robot)
+				V[closest_robot] = vcat(V[closest_robot], [ψdisc[i],λdisc[j]]) # This may not work immediately (may have to mess with transposes/dimensions)
+			else
+				V[closest_robot] = [ψdisc[i],λdisc[j]]
+			end
+		end
+	end
+	return V
+end
+
+"""
+function d
+	- computes distance between two points on the sphere 
+	- use haversine formula
+		- https://en.wikipedia.org/wiki/Haversine_formula?fbclid=IwAR1opVdva9xhRW5JuWO2RMV5_uRwX2M31VJFbRNVZ7Yx81nRF2z6-Ngz254
+
+	INPUTS:
+	- p₁: first point of the form (ψ,λ)
+	- p₂: second point of the form (ψ,λ)
+
+	OUTPUTS:
+	- dist: distance between the two points
+"""
+function d(p₁::Array{Real,1}, p₂::Array{Real,1})
+	d = 0 # Fill in!
+	return d
+end
+
+"""
+function compute_centroid
+	- compute the centroid of a Voronoi region based on its points
+
+	INPUTS:
+	- Vᵢ: Array of points (columns (ψ,λ)) in the Voronoi region	of robot i
+
+	OUTPUTS:
+	- CVᵢ: Centroid of the Voronoi region in the form (ψ,λ)
+"""
+function compute_centroid(Vᵢ::Array{Real,2})
+	CVᵢ = [0 0] # Fill in!
+	return CVᵢ
+end
+
